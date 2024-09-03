533e60e7f2ab9fe8ac380d2251ef5f9bc06c33b4
Silvanos Eric <ericoderito@gmail.com>
2024-09-03 19:49:22 +0300

    feat(oop): add Car class with attributes and display_info method
    - Added Car class with make, model, and year attributes.
    - Implemented display_info method to print car details.

---
+++ modules/oop.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"Car Make: {self.make}")
+        print(f"Car Model: {self.model}")
+        print(f"Car Year: {self.year}")
